7d5ca284d37b1995909e802cb2762a3af339aa1c
PlayerG9 <g9yout@gmail.com>
2024-04-30 15:53:43 +0200

    fixed escape function

--- src/roff/_util.py
+++ src/roff/_util.py
@@ -2,16 +2,14 @@
 r"""
 
 """
-import typing as t
-if t.TYPE_CHECKING:
-    from markdown_it import MarkdownIt
+import re
+import markdown_it
 
 
 __all__ = ['get_parser', 'escape']
 
 
-def get_parser() -> 'MarkdownIt':
-    import markdown_it
+def get_parser() -> 'markdown_it.MarkdownIt':
     from ._markdown import markdown_plugin_command
 
     parser = markdown_it.MarkdownIt()
@@ -19,8 +17,19 @@ def get_parser() -> 'MarkdownIt':
     return parser
 
 
-def escape(text: str, *, _escapes: str = '"\'.\\') -> str:
-    return text.translate(str.maketrans({
-        c: f"\\{c}"
-        for c in _escapes
-    }))
+ESCAPE_RULES = {
+    re.compile(r'\\'): r"\\\\",  # backslash
+    re.compile(r'\''): r"\[ap]",  # apostrophe
+    re.compile(r'\"'): r"\[dq]",  # double-quotation
+    re.compile(r'(?<=\d)-(?=\d)'): r"\[en]",  # en-dash
+    re.compile(r'(?<=\w)-(?=\w)'): r"\[em]",  # em-dash
+    re.compile(r'\^'): r"\[ha]",  # circumflex accent
+    re.compile(r'~'): r"\[ti]",  # tilde
+    re.compile(r'-'): r"\-",  # minus sign
+}
+
+
+def escape(text: str) -> str:
+    for rule, replacement in ESCAPE_RULES.items():
+        text = rule.sub(replacement, text)
+    return text
