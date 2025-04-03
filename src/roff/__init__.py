53c4f568dbcb29f3bea403fdd3fbcfe11f4778e2
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2025-04-04 00:30:25 +0200

    v0.8.0

--- src/roff/__init__.py
+++ src/roff/__init__.py
@@ -6,10 +6,10 @@ r"""
 __author__ = "Barakudum"
 __copyright__ = "Copyright 2024, utility-toolbox"
 __credits__ = ["Barakudum"]
-__license__ = "GPLv3"
+__license__ = "GPL-3.0-or-later"
 __maintainer__ = "Barakudum"
 __email__ = None
 __status__ = "Development"  # Prototype, Development, Production
 __description__ = "python-based cli to convert markdown to the roff (man-pages) format"
-__version_info__ = (0, 7, 0)
+__version_info__ = (0, 8, 0)
 __version__ = '.'.join(str(_) for _ in __version_info__)
